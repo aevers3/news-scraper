3ed7d45b5c008d1a701140a07cf3426d1cf92bbe
aevers3 <aevers3@users.noreply.github.com>
2019-06-10 20:55:38 -0400

    routes added

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.get("/scrape", function (req, res) {
             // Checks for summaries stored in lists and paragraphs, then scrapes accordingly.
             // If neither, the summary will say there is no preview available. In these cases, there generally is not a summary.
 
-            result.title = $(this).children('div').text();
+            result.title = $(this).find('h2').text();
 
             if ($(this).find('ul').text()) {
                 result.summary = $(element).find('ul').text();
@@ -62,15 +62,13 @@ app.get("/scrape", function (req, res) {
             // Create a new Article using the `result` object built from scraping
             db.Article.create(result)
                 .then(function (dbArticle) {
-                    // View the added result in the console
                     console.log(dbArticle);
                 })
                 .catch(function (err) {
-                    // If an error occurred, log it
                     console.log(err);
                 });
         });
-        
+
         // Send a message to the client
         res.send("Scrape Complete");
     });
@@ -78,15 +76,54 @@ app.get("/scrape", function (req, res) {
 
 
 // Route for getting all Articles from the db
-
+app.get("/articles", function (req, res) {
+    db.Article.find({})
+        .then(function (dbArticle) {
+            // If we were able to successfully find Articles, send them back to the client
+            res.json(dbArticle);
+        })
+        .catch(function (err) {
+            // If an error occurred, send it to the client
+            res.json(err);
+        });
+});
 
 
 // Route for grabbing a specific Article by id, populate it with it's note
-
-
+app.get("/articles/:id", function(req, res) {
+    // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+    db.Article.findOne({ _id: req.params.id })
+      // ..and populate all of the notes associated with it
+      .populate("note")
+      .then(function(dbArticle) {
+        // If we were able to successfully find an Article with the given id, send it back to the client
+        res.json(dbArticle);
+      })
+      .catch(function(err) {
+        // If an error occurred, send it to the client
+        res.json(err);
+      });
+  });
 
 // Route for saving/updating an Article's associated Note
-
+app.post("/articles/:id", function(req, res) {
+    // Create a new note and pass the req.body to the entry
+    db.Note.create(req.body)
+      .then(function(dbNote) {
+        // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
+        // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+        // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+        return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+      })
+      .then(function(dbArticle) {
+        // If we were able to successfully update an Article, send it back to the client
+        res.json(dbArticle);
+      })
+      .catch(function(err) {
+        // If an error occurred, send it to the client
+        res.json(err);
+      });
+  });
 
 
 
